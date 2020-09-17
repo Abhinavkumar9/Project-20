97361b8cc902070a59d19a2c53c44ad2875708f0
Abhinavkumar9 <70757816+Abhinavkumar9@users.noreply.github.com>
2020-09-17 22:02:40 +0530

    Update sketch.js

--- p5.play-boilerplate-master (2)/p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master (2)/p5.play-boilerplate-master/sketch.js
@@ -20,12 +20,14 @@ function draw() {
 if(wall.x-car.x < (car.width+wall.width)/2)
 {
   car.velocityX=0;
-  var deformation=0.5 * weight *speed/22509;
+  var deformation=0.5 * weight * speed* speed/22509;
   if(deformation>180)
   {
     car.shapeColor=color(230,230,0);
   }
-if(deformation<180 && deformation>100) {
+
+  
+  if(deformation<180 && deformation>100) {
   car.shapeColor=color(230,230,0); 
 }
   
