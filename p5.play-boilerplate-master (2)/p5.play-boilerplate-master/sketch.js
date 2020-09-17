558f48cba1cdd31960b549c4ff6a30cb6dd307ee
Abhinavkumar9 <70757816+Abhinavkumar9@users.noreply.github.com>
2020-09-17 18:13:31 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master (2)/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,35 @@
+var car, wall;
+var speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+speed=random(22,25)
+weight=random(400,1500)
+
+ car=createSprite(50, 200, 50, 50);
+car.velocityX = speed;
+
+wall=createSprite(1500,200,60,height/2);
+wall.shapeColor=(80,80,80);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+if(wall.x-car.x < (car.width+wall.width)/2)
+{
+  car.velocityX=0;
+  var deformation=0.5 * weight *speed/22509;
+  if(deformation>180)
+  {
+    car.shapeColor=color(230,230,0);
+  }
+
+  if(deformation<100)
+  {
+    car.shapeColor=color(0,255,0);
+  }
+  }
+  drawSprites();
+}
